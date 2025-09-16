2ceed0e0f75b2300bc0528b2c74398b455bc9070
Abdul Aziz <file.azis@gmail.com>
2025-09-16 10:03:27 +0000

    Error: ./node_modules/sql.js/dist/sql-wasm.js:94:15 [1m[31mModule not

--- next.config.ts
+++ next.config.ts
@@ -60,6 +60,7 @@ const nextConfig: NextConfig = {
         config.resolve.fallback = {
             ...config.resolve.fallback,
             fs: false,
+            path: false,
         };
     }
 
