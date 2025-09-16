7df691822a89a81f9f1edafb1f7c1d2b52c169ba
Abdul Aziz <file.azis@gmail.com>
2025-09-16 13:19:28 +0000

    Try fixing this error: `Runtime Error: Error: DropdownMenuTrigger is not

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -14,6 +14,7 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuShortcut,
+  DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Users, User, CreditCard, Settings, LifeBuoy, LogOut } from 'lucide-react';
 import { useUsers } from '@/hooks/use-users';
