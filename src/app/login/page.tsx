413a97e127d9f140c86c36d0500f35e53e40b584
Abdul Aziz <file.azis@gmail.com>
2025-10-01 09:40:24 +0000

    here is the image url link "https://i.imgur.com/Q7axrSm.png"

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,7 @@
 'use client';
 
 import * as React from 'react';
+import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import {
   Card,
@@ -46,21 +47,12 @@ export default function LoginPage() {
       <Card>
         <CardHeader className="text-center">
           <div className="flex justify-center items-center mb-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="h-8 w-8 text-primary"
-            >
-              <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
-              <path d="M12 11v4" />
-              <path d="M12 11a2 2 0 0 1 4 0v0" />
-              <path d="M12 11a2 2 0 0 0-4 0v0" />
-            </svg>
+            <Image
+              src="https://i.imgur.com/Q7axrSm.png"
+              alt="CipherWallet Logo"
+              width={48}
+              height={48}
+            />
           </div>
           <CardTitle className="text-2xl font-bold">Welcome to CipherWallet</CardTitle>
           <CardDescription>Sign in with your Google account to continue</CardDescription>
